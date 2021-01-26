dbcf79fe495179eef3a4e16b0c16e7bee35ee6d9
chaotic-bruno <46114370+chaotic-bruno@users.noreply.github.com>
2021-01-26 20:35:21 +0100

    Change base_footprint calculation for use without odom

--- src/converters/nao_footprint.hpp
+++ src/converters/nao_footprint.hpp
@@ -39,19 +39,19 @@ namespace nao
 
 inline void addBaseFootprint( boost::shared_ptr<tf2_ros::Buffer> tf2_buffer, std::vector<geometry_msgs::TransformStamped>& tf_transforms, const ros::Time& time )
 {
-  bool canTransform = tf2_buffer->canTransform("odom", "l_sole", time, ros::Duration(0.1) );
+  bool canTransform = tf2_buffer->canTransform("base_link", "l_sole", time, ros::Duration(0.1) );
   if (!canTransform)
   {
     ROS_ERROR_STREAM("Do not calculate NAO Footprint, no transform possible " << time);
     return;
   }
 
-  geometry_msgs::TransformStamped tf_odom_to_base, tf_odom_to_left_foot, tf_odom_to_right_foot;
+  geometry_msgs::TransformStamped tf_base_to_left_foot, tf_base_to_right_foot;
   try {
     // TRANSFORM THEM DIRECTLY INTO TRANSFORM
-    tf_odom_to_left_foot  = tf2_buffer->lookupTransform("odom", "l_sole",    time );
-    tf_odom_to_right_foot = tf2_buffer->lookupTransform("odom", "r_sole",    time );
-    tf_odom_to_base       = tf2_buffer->lookupTransform("odom", "base_link", time );
+    tf_base_to_left_foot  = tf2_buffer->lookupTransform("base_link", "l_sole",    time );
+    tf_base_to_right_foot = tf2_buffer->lookupTransform("base_link", "r_sole",    time );
+    //tf_odom_to_base       = tf2_buffer->lookupTransform("odom", "base_link", time );
   } catch (const tf2::TransformException& ex){
     ROS_ERROR("NAO Footprint error %s",ex.what());
     return ;
@@ -59,17 +59,18 @@ inline void addBaseFootprint( boost::shared_ptr<tf2_ros::Buffer> tf2_buffer, std
   // middle of both feet
   // z = fix to the lowest foot
   tf2::Vector3 new_origin(
-      float(tf_odom_to_right_foot.transform.translation.x + tf_odom_to_left_foot.transform.translation.x)/2.0,
-      float(tf_odom_to_right_foot.transform.translation.y + tf_odom_to_left_foot.transform.translation.y)/2.0,
-      std::min(tf_odom_to_left_foot.transform.translation.z, tf_odom_to_right_foot.transform.translation.z)
+      float(tf_base_to_right_foot.transform.translation.x + tf_base_to_left_foot.transform.translation.x)/2.0,
+      float(tf_base_to_right_foot.transform.translation.y + tf_base_to_left_foot.transform.translation.y)/2.0,
+      std::max(tf_base_to_left_foot.transform.translation.z, tf_base_to_right_foot.transform.translation.z)
       );
 
   // adjust yaw according to torso orientation, all other angles 0 (= in z-plane)
-  double yaw = helpers::transform::getYaw( tf_odom_to_base.transform ) ;
-  tf2::Quaternion new_q;
-  new_q.setRPY(0.0f, 0.0f, yaw);
-  tf2::Transform tf_odom_to_footprint( new_q, new_origin);
+  /* double yaw = helpers::transform::getYaw( tf_odom_to_base.transform ) ;
+  tf2::Quaternion new_q;*/
+  new_q.setRPY(0.0f, 0.0f, 0.0f); 
+  tf2::Transform tf_base_to_foot( new_q, new_origin);
 
+  /*
   // way too complicated here, there should be proper conversions!
   tf2::Quaternion q( tf_odom_to_base.transform.rotation.x,
                      tf_odom_to_base.transform.rotation.y,
@@ -83,7 +84,7 @@ inline void addBaseFootprint( boost::shared_ptr<tf2_ros::Buffer> tf2_buffer, std
   tf2::Transform tf_odom_to_base_conv( q,r);
   //tf2::Transform tf_odom_to_base_conv;
   //tf2::convert( tf_odom_to_base.transform, tf_odom_to_base_conv );
-  tf2::Transform tf_base_to_footprint = tf_odom_to_base_conv.inverse() * tf_odom_to_footprint;
+  tf2::Transform tf_base_to_footprint = tf_odom_to_base_conv.inverse() * tf_odom_to_footprint;*/
 
   // convert it back to geometry_msgs
   geometry_msgs::TransformStamped message;
@@ -92,13 +93,13 @@ inline void addBaseFootprint( boost::shared_ptr<tf2_ros::Buffer> tf2_buffer, std
   message.header.frame_id = "base_link";
   message.child_frame_id = "base_footprint";
 
-  message.transform.rotation.x = tf_base_to_footprint.getRotation().x();
-  message.transform.rotation.y = tf_base_to_footprint.getRotation().y();
-  message.transform.rotation.z = tf_base_to_footprint.getRotation().z();
-  message.transform.rotation.w = tf_base_to_footprint.getRotation().w();
-  message.transform.translation.x = tf_base_to_footprint.getOrigin().x();
-  message.transform.translation.y = tf_base_to_footprint.getOrigin().y();
-  message.transform.translation.z = tf_base_to_footprint.getOrigin().z();
+  message.transform.rotation.x = tf_base_to_foot.getRotation().x();
+  message.transform.rotation.y = tf_base_to_foot.getRotation().y();
+  message.transform.rotation.z = tf_base_to_foot.getRotation().z();
+  message.transform.rotation.w = tf_base_to_foot.getRotation().w();
+  message.transform.translation.x = tf_base_to_foot.getOrigin().x();
+  message.transform.translation.y = tf_base_to_foot.getOrigin().y();
+  message.transform.translation.z = tf_base_to_foot.getOrigin().z();
 
   //tf::transformTFToMsg( tf_base_to_footprint, message.transform);
   // publish transform with parent m_baseFrameId and new child m_baseFootPrintID
